c4b14c7cf2b5ccb9525d2a8273645c00523b0647
HORA <131666110+hamzaa11@users.noreply.github.com>
2023-12-01 16:41:28 +1300

    Add files via upload

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ from models import db, User, Movie, user_movie
 
 
 app = Flask(__name__, static_url_path='/static', static_folder='static')
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql//:'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# app.config['SECRET_KEY'] = '84857457'
-# db.init_app(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = '84857457'
+db.init_app(app)
 
 
 @app.route('/')
@@ -18,83 +18,83 @@ def home():
     return render_template('home.html',logged_in=logged_in, show_error_signin=show_error_signin)
 
 
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     email = request.form.get('email')
-#     password = request.form.get('password')
-#     print(password)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    print(password)
 
-#     checked_mail = User.query.filter_by(email=email).first()
+    checked_mail = User.query.filter_by(email=email).first()
 
-#     print(checked_mail)
-#     if checked_mail:
-#         checked_pass = check_password_hash(checked_mail.password, password)
-#         if checked_pass:
-#             logged_in = True
-#             return redirect(url_for('home', logged_in=logged_in))
-#         else: 
-#             flash('Password or Email invaild', category='Password')
-#             show_error = True
-#             show_login = True
-#             return redirect(url_for('home', show_login=show_login, show_error=show_error)) 
-#     else:
-#         flash('Password or Email invaild', category='Email')
-#         show_login = True
-#         show_error = True
-#         return redirect(url_for('home', show_login=show_login, show_error=show_error)) 
+    print(checked_mail)
+    if checked_mail:
+        checked_pass = check_password_hash(checked_mail.password, password)
+        if checked_pass:
+            logged_in = True
+            return redirect(url_for('home', logged_in=logged_in))
+        else: 
+            flash('Password or Email invaild', category='Password')
+            show_error = True
+            show_login = True
+            return redirect(url_for('home', show_login=show_login, show_error=show_error)) 
+    else:
+        flash('Password or Email invaild', category='Email')
+        show_login = True
+        show_error = True
+        return redirect(url_for('home', show_login=show_login, show_error=show_error)) 
        
-# @app.route('/signin', methods=['GET', 'POST'])
-# def signin():
-#     if request.method == 'POST':
-#         username = request.form.get('username')
-#         email = request.form.get('email')
-#         password = request.form.get('password')
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
 
-#         if email and username and password:
-#             check_email = User.query.filter_by(email=email).first()
-#             check_username = User.query.filter_by(username=username).first()
-#             short_password = len(password) < 8
-#             no_pass_charcter =  " " in password
+        if email and username and password:
+            check_email = User.query.filter_by(email=email).first()
+            check_username = User.query.filter_by(username=username).first()
+            short_password = len(password) < 8
+            no_pass_charcter =  " " in password
 
-#             if check_email or check_username or short_password or no_pass_charcter:
-#                 show_error_signin = True
-#                 show_vaild_signin = False
-#                 if short_password or no_pass_charcter:
-#                     if short_password:
-#                         flash('Password is too short', category='passwordSigninerror')
-#                     if no_pass_charcter:
-#                         flash('No spaces', category='passwordSigninerror')
-#                 else:
-#                     flash('✔', category='passwordSigninvaild')
-#                     show_vaild_signin = True
+            if check_email or check_username or short_password or no_pass_charcter:
+                show_error_signin = True
+                show_vaild_signin = False
+                if short_password or no_pass_charcter:
+                    if short_password:
+                        flash('Password is too short', category='passwordSigninerror')
+                    if no_pass_charcter:
+                        flash('No spaces', category='passwordSigninerror')
+                else:
+                    flash('✔', category='passwordSigninvaild')
+                    show_vaild_signin = True
                 
-#                 if check_email:
-#                     flash('Already exists', category='emailSigninerror')
-#                 else:
-#                     flash('✔', category='emailSigninvaild')
-#                     show_vaild_signin = True
+                if check_email:
+                    flash('Already exists', category='emailSigninerror')
+                else:
+                    flash('✔', category='emailSigninvaild')
+                    show_vaild_signin = True
                 
-#                 if check_username:
-#                     flash('Already exists', category='usernameSigninerror')
+                if check_username:
+                    flash('Already exists', category='usernameSigninerror')
                     
-#                 else:
-#                     flash('✔', category='usernameSigninvaild')
-#                     show_vaild_signin = True
+                else:
+                    flash('✔', category='usernameSigninvaild')
+                    show_vaild_signin = True
 
-#                 return redirect(url_for('home', show_error_signin=show_error_signin,show_vaild_signin=show_vaild_signin))
+                return redirect(url_for('home', show_error_signin=show_error_signin,show_vaild_signin=show_vaild_signin))
 
-#             else:
-#                 hashpass = generate_password_hash(password)
-#                 new_user = User(username=username, email=email, password=hashpass)
-#                 logged_in = True
-#                 with app.app_context():
-#                     db.session.add(new_user)
-#                     db.session.commit()
-#                 return redirect(url_for('home',logged_in=logged_in))
-#         else:
-#             flash('Please fill all fields', category='genralerror')
-#             show_error_signin = True
-#             return redirect(url_for('home', show_error_signin=show_error_signin))
+            else:
+                hashpass = generate_password_hash(password)
+                new_user = User(username=username, email=email, password=hashpass)
+                logged_in = True
+                with app.app_context():
+                    db.session.add(new_user)
+                    db.session.commit()
+                return redirect(url_for('home',logged_in=logged_in))
+        else:
+            flash('Please fill all fields', category='genralerror')
+            show_error_signin = True
+            return redirect(url_for('home', show_error_signin=show_error_signin))
 
 
 
