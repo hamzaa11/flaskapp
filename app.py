df8d4fd0921bde1f61a96cdef957f1ea28752d89
HORA <131666110+hamzaa11@users.noreply.github.com>
2023-12-10 09:30:20 +1300

    Add files via upload

--- app.py
+++ app.py
@@ -15,9 +15,9 @@ db.init_app(app)
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(5000), unique=True, nullable=False)
-    email = db.Column(db.String(12000), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    username = db.Column(db.String(5000000), unique=True, nullable=False)
+    email = db.Column(db.String(12000000), unique=True, nullable=False)
+    password = db.Column(db.String(1000000), nullable=False)
 
 
 @app.route('/')
