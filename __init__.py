ffa8206fee9473cf4ee0a98e4e51e4c51f8c753d
HORA <131666110+hamzaa11@users.noreply.github.com>
2023-12-13 13:56:20 +1300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@ import os
 
 app = Flask(__name__, static_url_path='/static', static_folder='static')
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATAASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
